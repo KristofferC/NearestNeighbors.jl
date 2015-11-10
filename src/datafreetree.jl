faa9e360e32fd6805c467a94641702f79f7c431c
Rene Donner <code@donner.at>
2015-11-10 14:55:12 +0100

    add tests for exceptions for DataFreeTree

--- src/datafreetree.jl
+++ src/datafreetree.jl
@@ -11,11 +11,11 @@ end
 
 function injectdata{T,M}(datafreetree::DataFreeTree{T,M}, data::Matrix{T})
     if size(data) != datafreetree.size
-        error("NearestNeighbors:injectdata: The size of 'data' $(data) does not match the data array used to construct the tree $(datafreetree.size).")
+        throw(DimensionMismatch("NearestNeighbors:injectdata: The size of 'data' $(data) does not match the data array used to construct the tree $(datafreetree.size)."))
     end
 
     if hash(data) != datafreetree.hash
-        error("NearestNeighbors:injectdata: The hash of 'data' does not match the hash of the data array used to construct the tree.")
+        throw(ArgumentError("NearestNeighbors:injectdata: The hash of 'data' does not match the hash of the data array used to construct the tree."))
     end
 
     typ = typeof(datafreetree.tree)
--- test/datafreetree.jl
+++ test/datafreetree.jl
@@ -1,3 +1,8 @@
 @testset "datafreetree" begin
-    @test_throws error(1)
+    data = rand(2,100)
+    data2 = rand(2,100)
+    data3 = rand(3,100)
+    t = DataFreeTree(KDTree, data)
+    @test_throws ArgumentError injectdata(t, data2) 
+    @test_throws DimensionMismatch injectdata(t, data3) 
 end
