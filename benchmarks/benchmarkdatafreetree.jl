1cb6645c9a64154fc5a1c4e98109aa55549762a8
Rene Donner <code@donner.at>
2015-11-16 10:19:39 +0100

    update benchmark script for reorder

--- benchmarks/benchmarkdatafreetree.jl
+++ benchmarks/benchmarkdatafreetree.jl
@@ -1,23 +1,51 @@
 using NearestNeighbors
+using Benchmarks
 
 runtimes = []
 runtimesreordered = []
-for n in [10, 10, 100, 1_000, 10_000, 100_000, 1_000_000]
-    dfilename = tempname()
-    rfilename = tempname()
+
+function create_tree(n, reorder=false)
+    filename = tempname()
     d = 10
-    data = Mmap.mmap(dfilename, Matrix{Float32}, (d, n))
+    data = Mmap.mmap(filename, Matrix{Float32}, (d, n))
     data[:] = rand(Float32, d, n)
-    reorderbuffer = Mmap.mmap(rfilename, Matrix{Float32}, (d, n))
-    t = injectdata(DataFreeTree(KDTree, data), data)
-    tr = injectdata(DataFreeTree(KDTree, data, reorderbuffer = reorderbuffer), reorderbuffer)
+    if reorder
+        reorderbuffer = Mmap.mmap(filename, Matrix{Float32}, (d, n))
+        t = injectdata(DataFreeTree(KDTree, data, reorderbuffer = reorderbuffer), reorderbuffer)
+    else
+        t = injectdata(DataFreeTree(KDTree, data), data)
+    end
 
-    r = @elapsed knn(t, data[:,rand(1:n, 1000)], 3)
-    push!(runtimes, r)
-
-    r = @elapsed knn(tr, data[:,rand(1:n,1000)], 3)
-    push!(runtimesreordered, r)
+    return t, data, filename
 end
-println("Speedup of reordered over unordered:")
-println(runtimes[2:end]./runtimesreordered[2:end])
 
+function knnbench(tree, data, n, N)
+    ind = rand(1:n, N)
+    knn(tree, data[:,ind], 3)[2]
+end
+
+function bench()
+    runtimes = []
+    runtimesreordered = []
+    ns = [10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000]
+    for n in ns
+        t, data, filename = create_tree(n)
+        tr, datar, filenamer = create_tree(n, true)
+
+        bm = @benchmark knnbench(t, data, n, 1000)
+        push!(runtimes, mean(bm.samples.elapsed_times) / 1e9)
+
+        bmr = @benchmark knnbench(tr, datar, n, 1000)
+        push!(runtimesreordered, mean(bmr.samples.elapsed_times) / 1e9)
+
+        rm(filename)
+        rm(filenamer)
+    end
+
+    println("Speedups through reordering:")
+    for i in 1:length(ns)
+        println("$(ns[i]): $(runtimes[i] ./ runtimesreordered[i])")
+    end
+    return
+end
+bench()
