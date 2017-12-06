c9110ad8f048be9e0566a3bf4f3036ba1268a339
Pablo San-Jose <lekand@gmail.com>
2017-12-06 14:50:39 +0100

    convert to Vector in BruteTree builder

--- src/brute_tree.jl
+++ src/brute_tree.jl
@@ -11,7 +11,7 @@ Creates a `BruteTree` from the data using the given `metric`.
 """
 function BruteTree(data::AbstractVector{V}, metric::Metric = Euclidean();
                    reorder::Bool=false, leafsize::Int=0, storedata::Bool=true) where {V <: AbstractVector}
-    BruteTree(storedata ? data : Vector{V}(), metric, reorder)
+    BruteTree(storedata ? convert(Vector{V}, data) : Vector{V}(), metric, reorder)
 end
 
 function BruteTree(data::Matrix{T}, metric::Metric = Euclidean();
