8b3ec5ce27e894d056051f6999005b18c656c77b
Nicolau Leal Werneck <nwerneck@gmail.com>
2021-10-01 22:32:13 +0200

    fixing old test code to proper patch

--- test/test_specialfloats.jl
+++ test/test_specialfloats.jl
@@ -4,7 +4,6 @@ using Test
 # Test for issue #78
 @testset "infs on data" begin
     for _ in 1:11
-
         coords = [
             29882.5 25974.3 Inf  Inf 17821.8 Inf Inf Inf Inf Inf 16322.0;
             9279.86 9286.35 Inf Inf 10320.4 Inf Inf Inf Inf Inf 11459.0;
@@ -20,22 +19,14 @@ end
 # Test for issue #125
 @testset "infs on data" begin
     for _ in 1:111
-
         Ndim = 35
         Npt = 408
 
         data = randn(Ndim, Npt)
-
-        data[:,1] .= Inf
-        # tree = KDTree(data)
-        tree = BallTree(data)
+        tree = KDTree(data)
 
         pointnan = repeat([NaN], Ndim)
         indnan,distnan = nn(tree, pointnan)
-        # @test 1 <= indnan <= Npt
-
-        pointinf = repeat([Inf], Ndim)
-        indinf, distinf = nn(tree, pointinf)
-        @test 1 <= indinf <= Npt
+        @test 1 <= indnan <= Npt
     end
 end
