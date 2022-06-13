14a047259b4ba0eebd45ecc6d0c7a3aa583465ae
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-06-13 10:36:23 +0200

    fix filtering out skipped points

--- src/knn.jl
+++ src/knn.jl
@@ -11,15 +11,15 @@ end
 Performs a lookup of the `k` nearest neigbours to the `points` from the data
 in the `tree`. If `sortres = true` the result is sorted such that the results are
 in the order of increasing distance to the point. `skip` is an optional predicate
-to determine if a point that would be returned should be skipped based on its 
+to determine if a point that would be returned should be skipped based on its
 index.
 """
 function knn(tree::NNTree{V}, points::Vector{T}, k::Int, sortres=false, skip::F=always_false) where {V, T <: AbstractVector, F<:Function}
     check_input(tree, points)
     check_k(tree, k)
     n_points = length(points)
-     dists = [Vector{get_T(eltype(V))}(undef, k) for _ in 1:n_points]
-     idxs = [Vector{Int}(undef, k) for _ in 1:n_points]
+    dists = [Vector{get_T(eltype(V))}(undef, k) for _ in 1:n_points]
+    idxs = [Vector{Int}(undef, k) for _ in 1:n_points]
     for i in 1:n_points
         knn_point!(tree, points[i], sortres, dists[i], idxs[i], skip)
     end
@@ -30,6 +30,11 @@ function knn_point!(tree::NNTree{V}, point::AbstractVector{T}, sortres, dist, id
     fill!(idx, -1)
     fill!(dist, typemax(get_T(eltype(V))))
     _knn(tree, point, idx, dist, skip)
+    if skip !== always_false
+        skipped_idxs = findall(==(-1), idx)
+        deleteat!(idx, skipped_idxs)
+        deleteat!(dist, skipped_idxs)
+    end
     sortres && heap_sort_inplace!(dist, idx)
     if tree.reordered
         for j in eachindex(idx)
